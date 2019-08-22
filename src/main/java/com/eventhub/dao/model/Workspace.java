233eb28f1b23b7abfa515e7142181bdc2447d4e1
porumamilla <porumamilla_raghu@yahoo.com>
2019-08-22 13:38:17 -0400

    Changes

---
+++ src/main/java/com/eventhub/dao/model/Workspace.java
@@ -0,0 +1,18 @@
+package com.eventhub.dao.model;
+
+public class Workspace {
+	private String orgId;
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOrgId() {
+		return orgId;
+	}
+	public void setOrgId(String orgId) {
+		this.orgId = orgId;
+	}
+}
